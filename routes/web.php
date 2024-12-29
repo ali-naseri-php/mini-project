ca40460b90d553e5d43c151a2d52e049e2bab3e5
ali <ali.naseri.php@gmail.com>
2024-12-29 12:45:37 +0330

    add route auth

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/register', function () {
+    return view('auth.register');
+})->name('form.register');
+
 Route::get('/login', function () {
     return view('auth.login');
-});
+})->name('form.login');
+
+Route::post('/login', [\App\Http\Controllers\auth\loginController::class,'login'])->name('login');
+Route::get('/logout', [\App\Http\Controllers\auth\loginController::class,'logout'])->name('logout');
+Route::post('/register', [\App\Http\Controllers\auth\loginController::class,'register'])->name('register');
 
